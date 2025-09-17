08f6f92f645e730071745529a7e94d23d0424b55
kingscom <kingscom85@gmail.com>
2025-09-17 18:54:36 +0900

    init

--- next.config.js
+++ next.config.js
@@ -2,17 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
-  trailingSlash: true,
   images: {
     domains: [],
-    unoptimized: true,
   },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/edu_anl' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/edu_anl' : '',
 }
 
 module.exports = nextConfig
